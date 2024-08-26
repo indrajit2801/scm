092648b7275b3a03549c9997fc1eaf1301a96c6d
indrajit2801 <rathodindrajit24@gmail.com>
2024-08-26 14:40:54 +0530

    new sign up

--- src/main/java/com/scm/entities/SocialLink.java
+++ src/main/java/com/scm/entities/SocialLink.java
@@ -1,6 +1,8 @@
 package com.scm.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
@@ -18,7 +20,8 @@ import lombok.Setter;
 public class SocialLink {
 
     @Id
-    private String id;
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
     private String link;
     private String title;
 
